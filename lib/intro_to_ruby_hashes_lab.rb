84e4c3f720cd75690c9be207a81a0e84a8b87eae
masterful-array-3850 <crgammage@gmail.com>
2020-04-22 19:42:06 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,15 +1,16 @@
 def new_hash
   new_hash = Hash.new
 end
-require 'pry'
+
 def my_hash
-  binding.pry
   new_hash = {:firstkey => "value1"}
-  new_hash[0]
+  new_hash
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  new_hash = {:name => "Grace Hopper"}
+  new_hash
 end
 
 
